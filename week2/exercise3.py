cb72eec49658df62a436967dc0a6840d8f656769
付健伦 <goodjason@fujianlundeMacBook-Pro.local>
2018-04-17 15:15:04 +1000

    sjsjjs
    sjsjs

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,9 +7,11 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    pass
+    if a_number % 2 == 0:
+        return False
 
-
+else:
+    return True
 def fix_it(moves=True, should_move=True):
     """Decide what to do.
 
@@ -31,7 +33,10 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    pass
+    empty_list = []
+    for i in range(10)
+        empty_list.append('*')
+        return empty_list
 
 
 
@@ -64,7 +69,10 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    pass
+    empty_list = []
+    for i in range(10):
+        another_list = []
+        for j in 
 
 
 def loops_3():
@@ -180,7 +188,22 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    pass
+    pyramid_width = 9
+    odds = filter(lambda x: x % 2 !=0,range (pyramid_width))
+    outer_list = []
+    for stars in odds:
+        inner_list = []
+
+        space_count = int((pyramid_width - stars) / 2)
+
+        space_list = [' ' for i in range(space_count)
+        star_kist = ['*' for i in range (stars)]
+
+        inner_list.extend(space_list)
+        inner_list.extend(star_list)
+        inner_list.extend(space_list)
+        outer_list.append(inner_list)
+    return outer_list
 
 
 def lp(some_kind_of_list, exercise_name):
